ef9871a645f0b6e100483a49aaf187b5d7a7d042
Wildfireone <wildfireone@gmail.com>
2019-03-21 22:13:39 +0000

    fixing images

--- js/map.js
+++ js/map.js
@@ -9,36 +9,36 @@
 
 
 var CartoDB_Positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-	subdomains: 'abcd',
-	maxZoom: 19
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+  subdomains: 'abcd',
+  maxZoom: 19
 });
 
 var mymap = L.map('mapid').setView([57.1497, -2.0943], 13);
 CartoDB_Positron.addTo(mymap);
 
 
-$.getJSON( "/suffragettes/suffra2.json", function( data ) {
-  data.forEach(function(location){
+$.getJSON("/suffragettes/suffra2.json", function(data) {
+  data.forEach(function(location) {
     console.log(location)
     var m = L.marker([location.lat, location.lng]).addTo(mymap);
     var title = location["pin-title"]
     var loc = location['location']
     var desc = location['text']
-		var photoSrc = location['photos']
-		var photoSrcs = photoSrc.split(' ');
-		var photoString ="";
-		for(i=0; i<photoSrcs.length; i++){
-			photoString = photoString + "<img class='popupimg' src='"+photoSrcs[i]+"' />"
-		}
-		var popup = L.popup({
-	maxHeight: 300,
-  closeOnClick: false,
-  keepInView: true
-});
-		var content = "<div class='pop'><b>"+title+"</b><br>"+loc+"<br>"+desc+"<br>"+"<p>"+photoString+"</p></div>"
-		popup.setContent(content);
-		popup._updateLayout();
+    var photoSrc = location['photos']
+    var photoSrcs = photoSrc.split(' ');
+    var photoString = "";
+    for (i = 0; i < photoSrcs.length; i++) {
+      photoString = photoString + "<img class='popupimg' src='" + photoSrcs[i] + "' />"
+    }
+    var popup = L.popup({
+      maxHeight: 100,
+      closeOnClick: false,
+      keepInView: true
+    });
+    var content = "<div class='pop'><b>" + title + "</b><br>" + loc + "<br>" + desc + "<br>" + "<p>" + photoString + "</p></div>"
+    popup.setContent(content);
+    popup._updateLayout();
     m.bindPopup(popup)
   })
 });
